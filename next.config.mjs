39b6c15c0b481fcee8011268b1ef69d37a6db4a4
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2024-10-22 20:20:38 +0200

    👷 Refactor next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
+    output:"export",
+    basePath: '/todo-list-avanc-react-okabasele',
+    assetPrefix: '/todo-list-avanc-react-okabasele/', 
+    trailingSlash: true, 
 };
 
 export default nextConfig;
